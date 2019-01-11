2f65b3952b985ef0fcb46ac4b74568abbb98987c
Brenton <brentonjjenkins@gmail.com>
2019-01-10 20:51:50 -0800

    working on portfolio content section

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,22 +1,20 @@
 $(document).ready(function () {
 
     // Links
-    // $("#home-nav").on("click", function (event) {
-    //     $('html,body').animate({
-    //         scrollTop: $("#home-connect").offset().top - 60
-    //     }, 'slow');
-    // });
-
-    $("#home-nav").on('click', function () {
-        window.location = "http://localhost:3000/index.html";
+    $("#home-nav").on("click", function (event) {
+        $('html,body').animate({
+            scrollTop: $("#home-connect").offset().top - 60
+        }, 'slow');
     });
 
     $("#about-nav").on('click', function () {
         window.location = "http://localhost:3000/about.html";
     });
 
-    $("#contact-nav").on('click', function () {
-        window.location = "https://brenton-jenkins.com/";
+     $("#contact-nav").on("click", function (event) {
+        $('html,body').animate({
+            scrollTop: $("#footer-connect").offset().top - 60
+        }, 'slow');
     });
 
 });
