98a14faa32a9a35fda62fdfb2f16c014d19b733f
Brenton <brentonjjenkins@gmail.com>
2019-01-11 17:58:20 -0800

    logo

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 
     // Links
-    $("#home-nav").on("click", function (event) {
-        $('html,body').animate({
-            scrollTop: $("#home-connect").offset().top - 60
-        }, 'slow');
+    // $("#home-nav").on("click", function (event) {
+    //     $('html,body').animate({
+    //         scrollTop: $("#home-connect").offset().top - 60
+    //     }, 'slow');
+    // });
+
+    $("#home-nav").on('click', function () {
+        window.location = "http://localhost:3000/index.html";
     });
 
     $("#about-nav").on('click', function () {
