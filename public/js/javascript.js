e1a1006fe73d0a042ffc2f8e474ff4806eb2e41f
Brenton <brentonjjenkins@gmail.com>
2019-01-14 18:48:58 -0800

    working on other html pages and links

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
 
-    // Links
+    // Home Page Links
 
     // Home
     $("#home-nav").on("click", function (event) {
         $('html,body').animate({
-            scrollTop: $("#home-connect").offset().top - 60
+            scrollTop: $("#home-connect").offset().top
         }, 'slow');
     });
-
-    // $("#home-nav").on('click', function () {
-    //     window.location = "http://localhost:3000/index.html";
-    // });
 
     // Portfolio
     $("#portfolio-nav").on("click", function (event) {
@@ -19,27 +15,89 @@ $(document).ready(function () {
             scrollTop: $("#portfolio-connect").offset().top - 60
         }, 'slow');
     });
-
+    
     // About
     $("#about-nav").on('click', function () {
-        window.location = "http://localhost:3000/about.html";
+        window.location = "https://brenton-jenkins.com/about.html";
     });
 
-    // Contact
-    $("#contact-nav").on("click", function (event) {
+     // Contact
+     $("#contact-nav").on("click", function (event) {
         $('html,body').animate({
-            scrollTop: $("#footer-connect").offset().top - 60
+            scrollTop: $("#footer-connect").offset().top
         }, 'slow');
+    });
+
+    // Microgurt
+    $(".microgurt-thumb").on('click', function () {
+        window.location = "https://brenton-jenkins.com/microgurt.html";
+    });
+
+    // The Big 8
+    $(".big8-thumb").on('click', function () {
+        window.location = "https://brenton-jenkins.com/big8.html";
+    });
+
+    // Flexafont
+    $(".flexafont-thumb").on('click', function () {
+        window.location = "https://brenton-jenkins.com/flexafont.html";
+    });
+
+      // Tulum Cosmetics
+      $(".tulum-thumb").on('click', function () {
+        window.location = "https://brenton-jenkins.com/tulum.html";
     });
 
      // Antlers and Acorns
      $(".haley-thumb").on('click', function () {
-        window.open("https://www.antlersandacorns.com/", "_blank");
+        window.location = "https://brenton-jenkins.com/haley.html";
     });
 
     // New 2 Dev
     $(".new2dev-thumb").on('click', function () {
-        window.open("https://new2dev.com/", "_blank");
+        window.location = "https://brenton-jenkins.com/new2dev.html";
     });
 
+
+    // Portfolio Page Links
+
+    // Home
+    $("#home-nav-portfolio").on('click', function () {
+        window.location = "https://brenton-jenkins.com/";
+    });
+
+    // Portfolio
+    $("#portfolio-nav-portfolio").on('click', function () {
+        window.location = "https://brenton-jenkins.com/#portfolio-connect";
+    });
+    
+    // About
+    $("#about-nav").on('click', function () {
+        window.location = "https://brenton-jenkins.com/about.html";
+    });
+
+     // Contact
+     $("#contact-nav").on("click", function (event) {
+        $('html,body').animate({
+            scrollTop: $("#footer-connect").offset().top
+        }, 'slow');
+    });
 });
+
+
+
+
+
+
+
+/////////////////////
+
+     // Antlers and Acorns
+    //  $(".haley-thumb").on('click', function () {
+    //     window.open("https://www.antlersandacorns.com/", "_blank");
+    // });
+
+    // New 2 Dev
+    // $(".new2dev-thumb").on('click', function () {
+    //     window.open("https://new2dev.com/", "_blank");
+    // });
