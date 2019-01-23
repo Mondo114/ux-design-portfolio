496e9d6aef834fdc3ec9b985ef178c4d6ed626e1
Brenton <brentonjjenkins@gmail.com>
2019-01-23 14:16:10 -0800

    fixed links when middle clicking

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -28,36 +28,6 @@ $(document).ready(function () {
         }, 'slow');
     });
 
-    // Microgurt
-    $(".microgurt-thumb").on('click', function () {
-        window.location = "https://brenton-jenkins.com/microgurt.html";
-    });
-
-    // The Big 8
-    $(".big8-thumb").on('click', function () {
-        window.location = "https://brenton-jenkins.com/big8.html";
-    });
-
-    // Flexafont
-    $(".flexafont-thumb").on('click', function () {
-        window.location = "https://brenton-jenkins.com/flexafont.html";
-    });
-
-    // Tulum Cosmetics
-    $(".tulum-thumb").on('click', function () {
-        window.location = "https://brenton-jenkins.com/tulum.html";
-    });
-
-    // Antlers and Acorns
-    $(".haley-thumb").on('click', function () {
-        window.location = "https://brenton-jenkins.com/haley.html";
-    });
-
-    // New 2 Dev
-    $(".new2dev-thumb").on('click', function () {
-        window.location = "https://brenton-jenkins.com/new2dev.html";
-    });
-
 
     // Portfolio Page Links
 
@@ -90,21 +60,6 @@ $(document).ready(function () {
         }, 'slow');
     });
 
-    // Tech Startup Weekend
-    $(".startup-thumb").on('click', function () {
-        window.open("https://startupweekend.org/", "_blank");
-    });
-
-    // Antlers and Acorns
-    $(".antlersandacorns-thumb").on('click', function () {
-        window.open("https://www.antlersandacorns.com/", "_blank");
-    });
-
-    // New 2 Dev
-    $(".new2dev-site-thumb").on('click', function () {
-        window.open("https://new2dev.com/", "_blank");
-    });
-
 
     // About Page Links
 
@@ -113,11 +68,6 @@ $(document).ready(function () {
         $('html,body').animate({
             scrollTop: $("#about-connect").offset().top
         }, 'slow');
-    });
-
-    // My Web Development Portfolio
-    $(".webdev-thumb").on('click', function () {
-        window.open("https://brenton-jenkins-portfolio-2.herokuapp.com/", "_blank");
     });
 
 });
